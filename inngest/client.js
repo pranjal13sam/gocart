b3348df78c933de6848ccecf30e9e6335b6a385b
Pranjal <pandeypranjal264@gmail.com>
2025-10-31 12:41:25 +0530

    updated_client.js

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "gocart-ecommerce" });
+export const inngest = new Inngest({
+  id: "gocart-ecommerce",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
