718c50ec934ca6ae98da2629bac246bd586283dd
Pranjal <pandeypranjal264@gmail.com>
2025-10-31 12:47:28 +0530

    updated_alias

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,7 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client.js";
-import {
-  deleteUserData,
-  saveUserData,
-  updateUserData,
-} from "../../../inngest/functions.js";
+import { inngest } from "@/inngest/client";
+import { deleteUserData, saveUserData, updateUserData } from "@/inngest/functions";
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
